a7a16b87c4c456032e578bdcdf8ba1ba6af5216e
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-09-17 19:43:18 -0500

    Signos zodiacales implementados

--- cuarta.py
+++ cuarta.py
@@ -1,6 +1,11 @@
 from math import pi
+from numpy import array
 
 class ProyectX():
+    def __init__(self):
+        self.__FechaInicio = array([22, 21, 20, 21, 21, 21, 22, 23, 24, 23, 23, 23])
+        self.__SignosZodiacales = array(["Capricornio","Acuario","Piscis","Aries","Tauro","Geminis","Cancer","Leo","Virgo","Libra","Escorpio","Sagitario"])
+
 
     def Square(self, base, height):
         return (base * height)
@@ -10,6 +15,20 @@ class ProyectX():
 
     def Triangle(self, base, height):
         return (base * height) / 2
+
+    def Zodiaco(self, day, month):
+        temp = month - 1
+        if(self.__FechaInicio[month] < day):
+            temp = temp + 1
+        return "que eres: " + self.__SignosZodiacales[temp]
+
+    def Factorial(self, number):
+        i = result = 1
+        while(number > i):
+            result = result * (i + 1)
+            i = i + 1
+        return result
+
 
     
 
@@ -31,6 +50,10 @@ if(opcion >= 1 and opcion <=2):
     height = int(input("Digame la altura: "))
 elif(opcion == 3):
     radio = int(input("Digame cual es el radio: "))
+elif(opcion == 4):
+    day = int(input("Digame su dia: "))
+    month = int(input("Digame su mes: "))
+
 
 
 
@@ -40,6 +63,15 @@ elif(opcion == 2):
     result = miClass.Triangle(base, height)
 elif(opcion == 3):
     result = miClass.Circle(radio)
-
+elif(opcion == 4):
+    result = miClass.Zodiaco(day, month)
+elif(opcion == 5):
+    limite = 100
+    n = 0
+    e = 0
+    while n < limite:
+        e += 1/miClass.Factorial(n)
+        n = n + 1
+    result = e
 
 print("El resultado es: ", result)
